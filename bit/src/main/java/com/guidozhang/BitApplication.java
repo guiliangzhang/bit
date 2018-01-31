37d2b4c2ab5871fa280c17b3b7fa246c24e65b6e
zhanggl <969243156@qq.com>
2018-01-31 20:59:34 +0800

    bit

---
+++ bit/src/main/java/com/guidozhang/BitApplication.java
@@ -0,0 +1,12 @@
+package org.zerhusen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BitApplication.class, args);
+    }
+}
